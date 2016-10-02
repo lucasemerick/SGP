f8f2fe6b1be1208204091292e078f908706c6856
Lucas Emerick <lucas_emerick@outlook.com>
2016-10-02 15:33:49 -0400

    Reparos básicos

--- app/controllers/appController.php
+++ app/controllers/appController.php
@@ -11,14 +11,11 @@ class App extends Controller {
         $this->redirect = new RedirectorHelper();
         if ($this->auth->checkLogin('boolean') == FALSE) {
             $this->redirect->goToControllerAction('auth', 'login');
-        } else {
-            $this->db = new AppModel();
         }
     }
 
     public function Index_action() {
-        $turmas['turmas'] = $this->db->listaTurmas($_SESSION['userData']['cd_usuario']);
-        $this->view('app', $turmas);
+        $this->view('app');
     }
 
 }
